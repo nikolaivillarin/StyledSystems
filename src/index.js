f3fb3c2556273037abf7b29569c13e951ba436da
Nikolai Villarin <nikolai.villarin@gmail.com>
2020-02-02 21:18:58 -0500

    Completed routing tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import Users from './users'
+import Contact from './contact'
+import NotFound from './notfound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/users">Users</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+                </li>
+            </ul>
+            <hr />
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/users/:id" component={Users} />
+                <Route path="/contact" component={Contact} />
+                <Route component={NotFound} />
+            </Switch>            
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(routing, document.getElementById('root'))
